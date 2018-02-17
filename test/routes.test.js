22fe5e9ee04bc429bf876097d21da2b27c4c967f
Samuel Okoroafor <samuel.i.okoroafor@gmail.com>
2018-02-17 17:13:03 +0100

    remove beforeEach for now

--- test/routes.test.js
+++ test/routes.test.js
@@ -8,7 +8,6 @@ chai.use(chaiHttp)
 const expect = chai.expect;
 
 describe('Routes Tests: routes', () => {
-    beforeEach()
     describe('Route: GET /api/v1/cache/:key ', () => {
         it('should return a string value representing the cache entry for the given key', () => {
             return chai
